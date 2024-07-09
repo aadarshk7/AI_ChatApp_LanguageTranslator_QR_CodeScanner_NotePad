04b61f2442ffbca2bc3c99810e6233288619d7ae
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-07-09 22:53:07 +0545

    chatai

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       create: (_) => ChatProvider(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Mero ChatBot App',
+        title: 'Mero AI Chat App',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
