e5a67da7ab78652ff4ed391113c257264ca55207
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-07-10 20:12:51 +0545

    chatai

--- lib/QR_Code_Reader/qr_code_reader.dart
+++ lib/QR_Code_Reader/qr_code_reader.dart
@@ -1,16 +1,135 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'dart:io';
 
-class QR_Code_Reader extends StatelessWidget {
-  const QR_Code_Reader({super.key});
+class QR_Code_Reader extends StatefulWidget {
+  @override
+  _QR_Code_ReaderState createState() => _QR_Code_ReaderState();
+}
+
+class _QR_Code_ReaderState extends State<QR_Code_Reader> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+  String? qrText;
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
+        title: Text('QR Code Generator and Scanner'),
       ),
-      body: Text("QR Code Reader"),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 4,
+            child: _buildQrView(context),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  if (qrText != null)
+                    Text(
+                      'Scan Result: $qrText',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      _showQrCodeDialog(context);
+                    },
+                    child: Text('Generate QR Code'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildQrView(BuildContext context) {
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+        MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+        borderColor: Colors.red,
+        borderRadius: 10,
+        borderLength: 30,
+        borderWidth: 10,
+        cutOutSize: scanArea,
+      ),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        qrText = scanData.code;
+      });
+    });
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    if (!p) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('No permission')));
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  void _showQrCodeDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Generated QR Code'),
+          content: SizedBox(
+            width: 200,
+            height: 200,
+            child: QrImage(
+              data: 'https://flutter.dev',
+              version: QrVersions.auto,
+              size: 200.0,
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
