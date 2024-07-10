50767a2f455f1a90bf82d0d16d534527484fb02d
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-07-10 11:39:24 +0545

    chatai

---
+++ lib/LanguageTranslator/languagetranslator.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class LanguageTranslator extends StatelessWidget {
+  const LanguageTranslator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
