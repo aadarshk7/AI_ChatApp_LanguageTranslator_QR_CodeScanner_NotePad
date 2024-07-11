0005a7b9adfb581b5f3b518e63c609bac0b2465a
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-07-11 13:53:53 +0545

    chatAI

--- lib/LanguageTranslator/languagetranslator.dart
+++ lib/LanguageTranslator/languagetranslator.dart
@@ -4,6 +4,7 @@ import 'package:flutter_tts/flutter_tts.dart';
 import 'package:provider/provider.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/services.dart';
+import 'camera_scanner.dart'; // Import the CameraScanner class
 
 void main() {
   runApp(LanguageTranslator());
