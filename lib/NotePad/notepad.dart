328ae61934f7aaed226fd49d10a07d1b88661d78
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-07-11 14:20:14 +0545

    chatAI

--- lib/NotePad/notepad.dart
+++ lib/NotePad/notepad.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(NotePad());
 }
 
-class MyApp extends StatelessWidget {
+class NotePad extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
