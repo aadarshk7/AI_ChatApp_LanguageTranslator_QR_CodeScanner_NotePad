abd96c75fd2c63c688e3c6f95ddaf2135b315c3f
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-07-11 09:46:54 +0545

    chatAI

--- lib/LanguageTranslator/languagetranslator.dart
+++ lib/LanguageTranslator/languagetranslator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:translator/translator.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:provider/provider.dart';
